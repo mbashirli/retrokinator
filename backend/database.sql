6f4ab439ea4dd17d5fb96386bbefb4d674f99e30
Ali_Asl <48028559+Aslanbayli@users.noreply.github.com>
2022-10-22 04:16:27 -0400

    psql queries

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,20 @@
-CREATE DATABASE pernfoods;
+CREATE DATABASE retrokinator;
 
-CREATE TABLE food(
-    food_id SERIAL PRIMARY KEY,
-    description VARCHAR(255)
+CREATE TABLE Food (
+    f_id SERIAL PRIMARY KEY,
+    f_name VARCHAR(255),
+    f_description TEXT
+);
+
+CREATE TABLE Receipt (
+    r_id SERIAL PRIMARY KEY,
+    cuisine VARCHAR(255),
+    r_description TEXT,
+    ingredients TEXT,
+    food INTEGER REFERENCES Food NOT NULL
+);
+
+CREATE TABLE Question (
+    q_id SERIAL PRIMARY KEY,
+    body TEXT
 );
