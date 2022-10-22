6fad95eec3d78628265edf58195eda018eb07620
mbashirli <mbashirlee@gmail.com>
2022-10-22 16:21:08 -0400

    added multiple functions to backend

--- backend/controller/foodController.js
+++ backend/controller/foodController.js
@@ -7,14 +7,30 @@ const pool = require("../db");
 // @access  Private
 const getFoods = asyncHandler(async (req, res) => {
   try {
-    //const characters = await pool.query("SELECT * FROM character");
-    res.send("get characters");
-  } catch {
-    console.error("Error!");
+    const foods = await pool.query("SELECT * FROM food");
+    res.json(foods.rows);
+  } catch (err) {
+    throw new Error("New Error");
   }
 });
 
-// @desc    Post answer
+// @desc Add food
+// @route POST /api/foods
+// @access Private
+
+const addFoods = asyncHandler(async (req, res) => {
+  try {
+    const { f_name, f_description } = req.body;
+    const newFood = await pool.query(
+      `INSERT INTO food (f_name, f_description) VALUES ('${f_name}', '${f_description}' )`
+    );
+  } catch (err) {
+    res.status(400);
+    throw new Error(err);
+  }
+});
+
+// @desc    Post option
 // @route   POST /api/foods
 // @access  Private
 
@@ -25,9 +41,37 @@ const postOption = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Answer not provided");
   }
+
+  if (answer != "yes" && answer != "no") {
+    res.status(400);
+    throw new Error("Answer can only be yes or no");
+  }
+
+  try {
+    res.send(answer);
+    // input option inside postgresql temporary session table
+  } catch (err) {
+    res.status(400);
+    throw new Error("Error");
+  }
+});
+
+// @desc    GET receipts
+// @route   POST /api/foods
+// @access  Private
+
+const getReceipts = asyncHandler(async (req, res) => {
+  try {
+    res.send(req.params.id);
+    // get the receipt using the id and send it back
+  } catch {
+    res.status(400);
+  }
 });
 
 module.exports = {
   getFoods,
+  addFoods,
   postOption,
+  getReceipts,
 };
--- backend/routes/foodRoutes.js
+++ backend/routes/foodRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { getFoods, postOption } = require("../controller/foodController");
+const {
+  getFoods,
+  postOption,
+  getReceipts,
+  addFoods,
+} = require("../controller/foodController");
 
 router.get("/getFoods", getFoods);
+router.get("/getReceipts/:id", getReceipts);
 router.post("/sendOption", postOption);
+router.post("/addFoods", addFoods);
 
 module.exports = router;
