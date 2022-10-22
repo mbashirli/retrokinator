451b283970385186ef0f27abf6a148e5672b0fba
gulphater <dotanoobbich@mail.ru>
2022-10-22 16:23:57 -0400

    before pukk

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,50 +1,15 @@
 import {useState} from "react";
 import logo from "../assets/logo.png"
 import gramofon from "../assets/gran.png"
+import bgVideo from "../assets/home-hero.webm"
+
 export default function Header() { 
     return (
         <>
+            <video src={bgVideo}></video>
             <header className="Header">
-                <nav>
-                    <img src={logo} className="logo" alt="" />
-                </nav>
-                <button className="nes-btn is-success">Play</button>
-                <div className="nes-container with-title">
-                    <h3 className="title">Radios</h3>
-                    <div className="radios">
-                        <label>
-                            <input type="radio" className="nes-radio" />
-                            <span>Yes</span>
-                        </label>
-                        <label>
-                            <input type="radio" className="nes-radio" />
-                            <span>No</span>
-                        </label>
-                    </div>
-                    <div className="radios" >
-                        <label>
-                            <input type="radio" className="nes-radio" />
-                            <span>Yes</span>
-                        </label>
-                        <label>
-                            <input type="radio" className="nes-radio" />
-                            <span>No</span>
-                        </label>
-                    </div>
-                </div>
-                <div className="nes-container is-dark with-title" id="transparent">
-                    <h3 className="title">Recipe list</h3>
-                    <div className="lists"> 
-                        <ul className="nes-list is-circle">
-                            <li>Lamb</li>
-                            <li>Rice</li>
-                            <li>Tahini</li>
-                            <li>Hummus</li>
-                            <li>Potatoes</li>
-                        </ul>
-                    </div>
-                </div>
-                {/* <progress className="nes-progress is-success" value="50" max="100"></progress> */}
+                <img src={logo} className="logo" alt="Retrokinator" />
+                <h3>Retrokinator</h3>
             </header>
         </>
     );
