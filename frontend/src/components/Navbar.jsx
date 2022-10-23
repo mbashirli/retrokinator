f83a5b589f20a321b569b566d34cb2bc594f3be5
Ali_Asl <48028559+Aslanbayli@users.noreply.github.com>
2022-10-23 04:03:37 -0400

    state

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,15 @@
 
 export default function Navbar() {
     return (
+        <>
         <div className="navbar">
+        <div id="root"></div>
+            <a target="_blank" 
+            title="instagram/web__addict" 
+            href="https://www.instagram.com/web__addict/">
+                <i className="fab fa-instagram"></i>
+            </a>
         </div>
+        </>
     );
 }
