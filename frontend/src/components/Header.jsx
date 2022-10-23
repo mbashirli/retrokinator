ce47b05ff35522b8242f3f7c3467b07439f2e2af
gulphater <dotanoobbich@mail.ru>
2022-10-23 01:21:59 -0400

    Video is working

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,18 @@
 import {useState} from "react";
 import logo from "../assets/logo.png"
 import gramofon from "../assets/gran.png"
-import bgVideo from "../assets/home-hero.webm"
+import bgVideo from "../assets/home-hero.mp4"
+
 
 export default function Header() { 
     return (
-        <>
-            <video src={bgVideo}></video>
+    <>
+    <video src={bgVideo} autoPlay playsInline loop muted className="header-video"></video>
+            {/* <video className="header-video"  loop>
+                <source type="video/mp4" src={bgVideo}/>
+            </video> */}
             <header className="Header">
+            
                 <img src={logo} className="logo" alt="Retrokinator" />
                 <h3>Retrokinator</h3>
             </header>
