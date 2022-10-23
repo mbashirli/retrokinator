ec3e7396796de90a2f83d9ee68a4aa4286cbf156
mbashirli <mbashirlee@gmail.com>
2022-10-23 07:28:23 -0400

    connected backend and frontend

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,39 @@
-import {useState} from "react";
-import logo from "../assets/logo.png"
+import { useState } from "react";
+import logo from "../assets/logo.png";
 import gramofon from "../assets/gran.png";
-import PlayScreen from "./PlayScreen"
+import PlayScreen from "./PlayScreen";
 import Header_intro from "./Header_intro";
-import bgVideo from "../assets/home-hero.mp4"
+import bgVideo from "../assets/home-hero.mp4";
 
 export default function Header() {
-    const [flag, setFlag] = useState(false); 
-    return (
+  const [flag, setFlag] = useState(false);
+  return (
     <>
-    <video src={bgVideo} autoPlay playsInline loop muted className="header-video"></video>
-            <header className="Header">
-                <div className="title-button">
-                    { !flag ? <Header_intro/> : "" }
-                    { !flag ? <button id="play-btn" className="nes-btn is-success" onClick={() => setFlag(!flag)}>Play</button> : ""}
-                </div>
-                { flag ? <PlayScreen/> : "" }
-            </header>
-        </>
-    );
+      <video
+        src={bgVideo}
+        autoPlay
+        playsInline
+        loop
+        muted
+        className="header-video"
+      ></video>
+      <header className="Header">
+        <div className="title-button">
+          {!flag ? <Header_intro /> : ""}
+          {!flag ? (
+            <button
+              id="play-btn"
+              className="nes-btn is-success"
+              onClick={() => setFlag(!flag)}
+            >
+              Play
+            </button>
+          ) : (
+            ""
+          )}
+        </div>
+        {flag ? <PlayScreen /> : ""}
+      </header>
+    </>
+  );
 }
-
--- frontend/src/components/PlayScreen.jsx
+++ frontend/src/components/PlayScreen.jsx
@@ -1,20 +1,89 @@
-export default function PlayScreen() {
-    return (
-        <>
-            <div className="question-block">
-                <div className="question">
-                    <div id="typedtext">Which of the following <br/> tastes do you desire right now?</div>
-                </div>
+import { useState } from "react";
 
-                <div className="answers">
-                    <form action="submit">
-                        <button className="nes-btn is-primary" type="button">A. Salty, umami, and sweet</button>
-                        <button className="nes-btn is-warning" type="button">B. Toasted ham</button>
-                        <button className="nes-btn is-success" type="button">C. Grilled chicken</button>
-                        <button className="nes-btn is-error" type="button">D. Something Sweet</button>
-                    </form>
-                </div>
-            </div>
-        </>
-    );
+// class PlayScreen extends Component {
+//   constructor(props) {
+//     super(props);
+//     = { apiResponse: "", countkey: 0 };
+//   }
+
+//   buttonA() {
+//     // this.countkey += 1;
+//     // console.log(this.countkey);
+//     const requestOptions = {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({ answer: "A", id: 0 }),
+//     };
+//     fetch("https://localhost:4000/api/foods/sendOption", requestOptions)
+//       .then((response) => response.json())
+//       .then((data) => this.setState({ postId: data.id }));
+//   }
+
+//   render() {
+//     return (
+
+//     );
+//   }
+// }
+
+export default PlayScreen;
+
+function PlayScreen() {
+  let [count, setCount] = useState(0);
+  let [apiResponse, setState] = useState("");
+  function callAPI() {
+    let link = `http://localhost:4000/api/foods/getQuestions/${count}`;
+    fetch(link)
+      .then((res) => res.json())
+      .then((res) => setState(res));
+  }
+
+  function componentWillMount() {
+    callAPI();
+  }
+
+  callAPI();
+
+  return (
+    <>
+      <div className="question-block">
+        <div className="question">
+          <div id="typedtext">{apiResponse["q_body"]}</div>
+        </div>
+
+        <div className="answers">
+          <form action="submit">
+            <button
+              className="nes-btn is-primary"
+              type="button"
+              onClick={() => setCount(count + 1)}
+            >
+              {apiResponse["q_answer1"]}
+            </button>
+            <button
+              className="nes-btn is-warning"
+              type="button"
+              onClick={() => setCount(count + 1)}
+            >
+              {apiResponse["q_answer2"]}
+            </button>
+            <button
+              className="nes-btn is-success"
+              type="button"
+              onClick={() => setCount(count + 1)}
+            >
+              {apiResponse["q_answer3"]}
+            </button>
+            <button
+              className="nes-btn is-error"
+              type="button"
+              onClick={() => setCount(count + 1)}
+            >
+              {apiResponse["q_answer4"]}
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
