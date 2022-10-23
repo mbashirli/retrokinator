4355e535967e5baaf396ed6d660f0bbb06fa1b8a
Ali_Asl <48028559+Aslanbayli@users.noreply.github.com>
2022-10-23 05:28:04 -0400

    home-page-colors

--- frontend/src/components/Header_intro.jsx
+++ frontend/src/components/Header_intro.jsx
@@ -3,9 +3,9 @@ export default function Header_intro() {
         <>
         <div className="head-intro">
             <div className="title">
-                <div className="intro"><span style={{color: '#e74c3c', textShadow: '2px 2px #8e44ad'}}>Welcome </span>
-                <span style={{color: '#f1c40f', textShadow: '2px 2px #8e44ad'}} className="is-warning">to the </span>
-                <span style={{color: '#3498db', textShadow: '2px 2px #8e44ad'}}>RETROKINATOR.</span></div>
+                <div className="intro"><span style={{color: '#e74c3c', textShadow: '2px 2px #8c2022'}}>Welcome </span>
+                <span style={{color: '#f1c40f', textShadow: '2px 2px #e59400 '}} className="is-warning">to the </span>
+                <span style={{color: '#3498db', textShadow: '2px 2px #006bb3'}}>RETROKINATOR.</span></div>
             </div>
             <div className="desc">
                 <div className="text">If you are here that means you are unsure 
